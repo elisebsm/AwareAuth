54560b607cc5d33f6208bd6af866f86213765bf4
Elise2 <elisesmebye@gmail.com>
2021-03-11 14:55:25 +0100

    elise  opning av filer

--- app/src/main/java/com/example/testaware/IdentityHandler.java
+++ app/src/main/java/com/example/testaware/IdentityHandler.java
@@ -2,76 +2,174 @@ package com.example.testaware;
 
 import android.content.Context;
 
+import com.example.testaware.Contact;
+
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.security.Key;
 import java.security.KeyManagementException;
+import java.security.KeyPair;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Objects;
 
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
-import javax.sql.ConnectionEventListener;
 
 public class IdentityHandler {
     SSLContext sslContext;
 
-    public SSLContext getSSLContext(Context context) throws CertificateException, IOException, KeyStoreException, NoSuchAlgorithmException, KeyManagementException, UnrecoverableKeyException {
+
+    public static SSLContext getSSLContext(Context context){
 
         //FileInputStream inputStreamCertificate = context.openFileInput("rsacert.pem")
 
-// key manager
-        CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
 
-        String keyStoreType = KeyStore.getDefaultType();
-        KeyStore keyStore = KeyStore.getInstance(keyStoreType); //TODO keystoresype
-        FileInputStream fileInputStream = new FileInputStream("KeyStore"); //todo: get the right file (.p12 format? PKCS)
-        keyStore.load(fileInputStream, null); //TOdO: set password
+        try {
+            // key manager
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
 
-        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("X509");
-        keyManagerFactory.init(keyStore, null);
-        KeyManager[] keyManagers = keyManagerFactory.getKeyManagers();
+            //keystore containing certificate
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+            //FileInputStream fileInputStream = new FileInputStream("KeyStore");
+            FileInputStream fileInputStream = new FileInputStream("/data/data/com.example.testaware/files/keystore/keystore.p12"); //todo: get the right file (.p12 format? PKCS)
+            keyStore.load(fileInputStream, "Master2021".toCharArray());
+
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("X509");
+            keyManagerFactory.init(keyStore, null);
+            KeyManager[] keyManagers = keyManagerFactory.getKeyManagers();
 
 //trust manager
-        File caFile = getCA(context);
-        InputStream inputStreamCertificate = new BufferedInputStream(new FileInputStream(caFile)); //TODO close stream
-        X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(inputStreamCertificate);
-        String certificateAlias = certificate.getSubjectX500Principal().getName();
-        KeyStore keyStoreCA = KeyStore.getInstance(KeyStore.getDefaultType());
-        keyStoreCA.load(null);
-        keyStoreCA.setCertificateEntry(certificateAlias, certificate);
+            //File caFile = getCA(context); //TODO change filepath
+            File caFile = new File("/data/data/com.example.testaware/files/ca/root_ca.pem");
 
-        KeyManagerFactory keyManagerFactoryCA = KeyManagerFactory.getInstance("X509");
-        keyManagerFactory.init(keyStoreCA, null);
-        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("X509");
-        trustManagerFactory.init(keyStoreCA);
-        TrustManager [] trustManagers = trustManagerFactory.getTrustManagers();
+            InputStream inputStreamCertificate = null; //TODO close stream
+            inputStreamCertificate = new BufferedInputStream(new FileInputStream(caFile));
+            X509Certificate certificate = (X509Certificate) certificateFactory.generateCertificate(inputStreamCertificate);
+            String certificateAlias = certificate.getSubjectX500Principal().getName();
+            KeyStore keyStoreCA = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStoreCA.load(null);
+            keyStoreCA.setCertificateEntry(certificateAlias, certificate);
 
-        // Create an SSLContext that uses our TrustManager
-        sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(keyManagers, trustManagers, null);
-        return sslContext;
+            KeyManagerFactory keyManagerFactoryCA = KeyManagerFactory.getInstance("X509");
+            keyManagerFactoryCA.init(keyStoreCA, null);
+            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("X509");
+            trustManagerFactory.init(keyStoreCA);
+            TrustManager [] trustManagers = trustManagerFactory.getTrustManagers();
+
+            // Create an SSLContext that uses our TrustManager
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagers, trustManagers, null);
+            return sslContext;
+        } catch (IOException | CertificateException | NoSuchAlgorithmException | UnrecoverableKeyException | KeyStoreException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
 
     //TODO: tell the socket connection to use a socketfactory??
     // for http from android developer Tell the URLConnection to use a SocketFactory from our SSLContext
 
-    private File getCA(Context context){
+    private static File getCA(Context context){
+        File [] files = context.getExternalFilesDirs("ca")[0].listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".pem");
+            }
+        });
         //TODO: get ca file (.pem file)
+        return Objects.requireNonNull(files)[0];
+
+    }
+
+    private static File getPKCS(Context context){
+        File [] files = context.getExternalFilesDirs("keystore")[0].listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".pk12");
+            }
+        });
+        return null;
+    }
+
+
+    private static File [] getContactFile(Context context){
+        return context.getExternalFilesDirs("contacts")[0].listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".pem");
+            }
+        });
+    }
+
+    public static ArrayList<Contact> getContacts(Context context){
+        ArrayList<Contact> contacts = new ArrayList<>();
+        for (File contactFile: getContactFile(context)){
+            try {
+                FileInputStream fileInputStream = new FileInputStream(contactFile);
+                CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+                X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(fileInputStream);
+                contacts.add(new Contact(x509Certificate));
+            } catch (FileNotFoundException | CertificateException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return contacts;
+    }
+
+    public static KeyPair getKeyPair(Context context){
+        KeyStore keyStore = null;
+        try {
+            keyStore = KeyStore.getInstance("PKCS12");
+            //FileInputStream fileInputStream = new FileInputStream(getPKCS(context));
+
+            FileInputStream fileInputStream = new FileInputStream( new File("/data/data/com.example.testaware/files/keystore/keystore.p12"));
+
+            keyStore.load(fileInputStream, "Master2021".toCharArray());
+            Enumeration<String> stringEnumeration = keyStore.aliases();
+            String alias = "";
+            boolean isAliasWithPrivateKey = false;
+
+            while(stringEnumeration.hasMoreElements()){
+                alias = stringEnumeration.nextElement();
+                if(isAliasWithPrivateKey = keyStore.isKeyEntry(alias)){
+                    break;
+                }
+            }
+
+            if(isAliasWithPrivateKey) {
+                Key key = keyStore.getKey(alias, new char[0]);
+                if (key instanceof PrivateKey){
+                    Certificate certificate = keyStore.getCertificate(alias);
+                    PublicKey publicKey = certificate.getPublicKey();
+                    return new KeyPair(publicKey, (PrivateKey) key);
+                }
+            }
+        } catch (KeyStoreException | CertificateException | IOException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
