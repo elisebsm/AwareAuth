4efa1dcb9f17fa61ba2933de03fb0399d900c0a4
kirstenskaug <kirstenskaug@gmail.com>
2021-04-09 11:04:22 +0200

    Getting peer auth to work

---
+++ app/src/main/java/com/example/testaware/listeners/OnMessageReceivedListener.java
@@ -0,0 +1,8 @@
+package com.example.testaware.listeners;
+
+import com.example.testaware.models.Message;
+
+
+public interface OnMessageReceivedListener {
+    public void onMessagReceived(Message message);
+}
